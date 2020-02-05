f1326d29b3799f559692c0a6a587df3948fbc93a
Tan Nguyen <nguyendinhtan96@gmail.com>
2020-02-04 19:48:39 -0800

    Update clientapp

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -9,6 +10,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(cors());
 mongoose.connect('mongodb://localhost:27017/training3', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // view engine setup
@@ -22,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ config.js
@@ -0,0 +1,5 @@
+const HOST = "https://localhost:3000/"
+
+export default {
+    HOST
+}
