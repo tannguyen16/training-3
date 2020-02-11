8e784672d506430e11244076eb2d65a51d345513
Tan Nguyen <nguyendinhtan96@gmail.com>
2020-02-10 22:59:21 -0800

    Update calls

--- models/Users.js
+++ models/Users.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({ name: String, grade: Number });
 
-const User = mongoose.model("User", schema);
+const User = mongoose.model("Users", schema);
 
 module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,48 @@
 var express = require('express');
+var UserModel = require('../models/Users');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Student Grade' });
+router.post('/', async (req, res) => {
+  var newUser = new UserModel(req.body);
+
+  try {
+    await newUser.save();
+    res.redirect('/');
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.get('/dmindex', function(req, res, next) {
-  res.render('dmindex', { title: 'Student Grade - Dark Mode' });
+router.get('/', async function(req, res, next) {
+  let { sort, dir } = req.query;
+  dir = dir !== undefined ? dir : -1;
+  let sortQuery = {};
+    if (sort) {
+      sortQuery[sort] = dir;
+    }
+
+  const users = await UserModel.find().sort(sortQuery);
+  res.render('index', { title: 'Student Grade Table', users, sort, dir });
+});
+
+router.get('/delete/:id', async function(req, res, next) {
+  try {
+    const user = await UserModel.findByIdAndDelete(req.params.id);
+    if (!user) return;
+    res.redirect('/');
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/edit/:id', async function(req, res, next) {
+  try {
+    const user = await UserModel.findByIdAndDelete(req.params.id);
+    if (!user) return;
+    res.redirect('/');
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
